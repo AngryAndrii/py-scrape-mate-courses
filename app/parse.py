ed12d433c5d75218c00cc59067e80247c3604bc1
Andrii Chychkan <mywork1728@gmail.com>
2026-02-17 17:21:44 +0200

    initialized parser

--- app/parse.py
+++ app/parse.py
@@ -1,5 +1,7 @@
 from dataclasses import dataclass
+import requests
 
+URL = "https://mate.academy/"
 
 @dataclass
 class Course:
@@ -9,4 +11,18 @@ class Course:
 
 
 def get_all_courses() -> list[Course]:
-    pass
+    text = requests.get(URL).content
+    # print(text)
+    print("hello")
+
+
+
+def main():
+    get_all_courses()
+
+if __name__ == "__main__":
+    main()
+
+#div.ProfessionsListSectionTemplate_cardsWrapper__un6ny.ProfessionsListSectionTemplate_cardsWrapperShowAll__kWdCD > a:nth-child(2) > div.ProfessionCard_content__mPiVi > h3 > span
+#div.ProfessionsListSectionTemplate_cardsWrapper__un6ny.ProfessionsListSectionTemplate_cardsWrapperShowAll__kWdCD > a:nth-child(2) > div.ProfessionCard_content__mPiVi > p.typography_paragraphMedium__FWO7K.ProfessionCard_text___l0Du.ProfessionCard_description__K8weo
+#div.ProfessionsListSectionTemplate_cardsWrapper__un6ny.ProfessionsListSectionTemplate_cardsWrapperShowAll__kWdCD > a:nth-child(2) > div.ProfessionCard_content__mPiVi > p.typography_paragraphMedium__FWO7K.ProfessionCard_text___l0Du.ProfessionCard_duration__13PwX
