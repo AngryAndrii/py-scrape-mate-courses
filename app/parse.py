b02d7481a9c4fd62d57710c265d410fd1c5f1c3d
Andrii Chychkan <mywork1728@gmail.com>
2026-02-17 20:10:55 +0200

    finished

--- app/parse.py
+++ app/parse.py
@@ -1,4 +1,5 @@
-from dataclasses import dataclass
+import csv
+from dataclasses import dataclass, fields, astuple
 import requests
 from bs4 import BeautifulSoup, Tag
 
@@ -10,13 +11,14 @@ class Course:
     short_description: str
     duration: str
 
+COURSE_FIELDS = [field.name for field in fields(Course)]
 
 def parse_single_course(course: Tag) -> Course:
-    print(dict(
+    return Course(
         name=course.select_one("h3 span").text,
         short_description=course.select_one(".ProfessionCard_description__K8weo").text,
         duration=course.select_one(".ProfessionCard_duration__13PwX").text
-    ))
+    )
 
 def get_all_courses() -> list[Course]:
     text = requests.get(URL).content
@@ -24,13 +26,14 @@ def get_all_courses() -> list[Course]:
     cards = soup.select(".ProfessionCard_content__mPiVi")
     return [parse_single_course(card) for card in cards]
 
+def write_courser_to_csv(courses: list[Course]) -> None:
+    with open("result.csv", "w") as file:
+        writer = csv.writer(file)
+        writer.writerow(COURSE_FIELDS)
+        writer.writerows([astuple(course) for course in courses])
 
 def main():
-    get_all_courses()
+    write_courser_to_csv(get_all_courses())
 
 if __name__ == "__main__":
     main()
-
-#div.ProfessionsListSectionTemplate_cardsWrapper__un6ny.ProfessionsListSectionTemplate_cardsWrapperShowAll__kWdCD > a:nth-child(2) > div.ProfessionCard_content__mPiVi > h3 > span
-#div.ProfessionsListSectionTemplate_cardsWrapper__un6ny.ProfessionsListSectionTemplate_cardsWrapperShowAll__kWdCD > a:nth-child(2) > div.ProfessionCard_content__mPiVi > p.typography_paragraphMedium__FWO7K.ProfessionCard_text___l0Du.ProfessionCard_description__K8weo
-#div.ProfessionsListSectionTemplate_cardsWrapper__un6ny.ProfessionsListSectionTemplate_cardsWrapperShowAll__kWdCD > a:nth-child(2) > div.ProfessionCard_content__mPiVi > p.typography_paragraphMedium__FWO7K.ProfessionCard_text___l0Du.ProfessionCard_duration__13PwX
